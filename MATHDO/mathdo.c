c82048b73d53e806e76969382b447d115e754950
vzik <43172593+Kinazarova@users.noreply.github.com>
2019-11-08 15:25:02 +0600

    Update mathdo.c

--- MATHDO/mathdo.c
+++ MATHDO/mathdo.c
@@ -1,23 +1,42 @@
+#include <wait.h>
 #include <stdio.h>
-#include <sys/types.h>
 #include <unistd.h>
-#include <sys/wait.h>
+#include <stdlib.h>
+int main(int argc, char *argv[]){
+    
+	int r1 = fork();
+	
+	if (r1 == 0) {
+		execlp("PYTHON", "PYTHON_3", "add.py", argv[1], argv[2], NULL);
+		exit(0);
+	}    
+	int r2 = fork();
+	
+	if (r2 == 0) {
+		execlp("JAVA", "JAVA", "divide", argv[1], argv[2], NULL);
+		exit(0);
+	} 
+	int r3 = fork();
 
-int main(int argc, char* argv[]){
-	int pid = fork(); // child1
-	if (pid==0){ // child 1
-		execlp("./add.py",":)",argv[1],argv[2],NULL);
+	if (r3 == 0) {
+		execlp("NODEJS", "NODEJS", "minus.js", argv[1], argv[2], NULL);
+		exit(0);
+	}    
 
-	} else // parent
-	{
-		int pid2 = fork();
-		if (pid2==0){ // child 2
-			execlp("./div.sh",":)",argv[1],argv[2],NULL);
-		} else // parent
-		{
-			wait(0);
-			wait(0);
-			printf("parent: done\n");
-		}		
-	}
+	int r4 = fork();
+	
+	if (r4 == 0) {
+		execlp("BASH", "BASH", "multiply.sh", argv[1], argv[2], NULL);
+		exit(0);
+	} 
+    	waitpid(r1, NULL, 0);
+    	waitpid(r2, NULL, 0);
+    	waitpid(r3, NULL, 0);
+    	waitpid(r4, NULL, 0);
+	printf("PARENT: done.\n");
+	
+
+
+
 }
+
