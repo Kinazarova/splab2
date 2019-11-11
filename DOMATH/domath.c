be593d53649d6f46a09bf13abb3d83c9cf1ab381
vzik <43172593+Kinazarova@users.noreply.github.com>
2019-11-11 23:26:13 +0600

    Update domath.c

--- DOMATH/domath.c
+++ DOMATH/domath.c
@@ -1,1 +1,51 @@
-// your code here
+#include <stdio.h>
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <wait.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv){
+	if (argc<3) {
+		printf("TOO FEW ARGS.\n");
+		exit(1);
+	}
+	pid_t d = fork();
+	if(d == 0){
+		int q = atoi(argv[2]);
+		int z = atoi(argv[1]);
+		printf("CHAILD_4: %d / %d = %d\n",z,q,z/q);
+		exit(0);
+	}
+	
+	pid_t a = fork();
+
+	if(a == 0){
+		int q = atoi(argv[2]);
+		int z = atoi(argv[1]);
+		printf("CHAIL_1: %d + %d = %d\n",z,q,z+q);
+		exit(0);
+	}
+	pid_t c = fork();
+	if(c == 0){
+		int q = atoi(argv[2]);
+		int z = atoi(argv[1]);
+		printf("CHAILD_3: %d * %d = %d\n",z,q,z*q);
+		exit(0);
+	}
+	pid_t b = fork();
+	if(b == 0){
+		int q = atoi(argv[2]);
+		int z = atoi(argv[1]);
+		printf("CHAILD_2: %d - %d = %d\n",z,q,z-q);
+		exit(0);
+	}
+
+	waitpid(a, NULL, 0);
+    	waitpid(b, NULL, 0);
+    	waitpid(c, NULL, 0);
+    	waitpid(d, NULL, 0);
+
+	printf("PARENT_: done.\n");
+}
+
